84b932ff90b4a221e674a09c57a2eda567edaf92
Gabriel <lantzgabriel@gmail.com>
2015-06-10 10:26:25 +0200

    comment

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 General render app for CMS Node
 
 ## Install
-Run npm install in terminal
+Run npm install in terminal hehe
 
 ## Start
 Run npm run start in terminal
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('index', { name: 'Swig templates are here to stay : )' });
+  res.render('index', { name: 'Swig templates are here to stay : )', time: process.env.TIMES });
 });
 
 app.listen(app.get('port'), function() {
