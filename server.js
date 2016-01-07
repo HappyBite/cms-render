39c3268ea2fbae7943dfe10de8617dda50de8e2b
Gabriel <lantzgabriel@gmail.com>
2016-01-07 17:20:51 +0100

    New stuff

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var swig = require('swig');
 var filters = require('./swig-extensions/filters.js');
 var tags = require('./swig-extensions/tags.js');
@@ -28,6 +29,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use('/public', express.static(__dirname + '/public'));
 
 loadApplication(app);
+
+// the static middleware must come after the sass middleware
+var cacheFor = -1;
+if(process.env.PUBLIC) {
+  // Strange flickering effect when caching files. Explore whi this is happening.
+  cacheFor = 86400000; // one day
+}
+app.use(express.static( path.join( __dirname, 'public' ), { maxAge: cacheFor }));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
