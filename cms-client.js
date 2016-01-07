4424c4b3544bc95835c6175853b480a1d4746902
Gabriel <lantzgabriel@gmail.com>
2016-01-07 13:03:08 +0100

    New stuff

--- cms-client.js
+++ cms-client.js
@@ -1,10 +1,27 @@
 var cms = require('../sdk/index.js');
 
-var client = new cms.Client({
+var config = {};
+
+config = {
   store: '568e50ea2016b75641bfc3c2',
   accessToken: '568e50f22016b75641bfc3cd',
-  port: 8080,
-  secure: false
+  port: '80',
+  secure: true
+}
+
+// config = {
+//   store: '568e50ea2016b75641bfc3c2',
+//   accessToken: '568e50f22016b75641bfc3cd',
+//   port: '8080',
+//   secure: false
+// }
+
+
+var client = new cms.Client({
+  store: config.store,
+  accessToken: config.accessToken,
+  port: config.port,
+  secure: config.secure
 })
 
 module.exports = client;
