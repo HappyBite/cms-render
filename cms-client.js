830ff56924148ceb63655ed787cb356d7e261180
Gabriel <lantzgabriel@gmail.com>
2016-01-10 17:16:45 +0100

    New stuff

--- cms-client.js
+++ cms-client.js
@@ -1,15 +1,11 @@
-var cms = require('./bower_components/sdk/index.js');
-//var cms = require('../sdk/index.js');
-
-//console.log('Store ID: ' + process.env.STORE_ID);
-//console.log('Access Token: ' + process.env.ACCESS_TOKEN);
-
 if(process.env.PUBLIC) {
+  var cms = require('./bower_components/sdk/index.js');
   var client = new cms.Client({
     store: process.env.STORE_ID,
     accessToken: process.env.ACCESS_TOKEN
   });
 } else {
+  var cms = require('../sdk/index.js');
   var client = new cms.Client({
     host: 'localhost',
     store: process.env.STORE_ID,
