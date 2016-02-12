9c8fc7ab5c211e31d758dbde7385a09cf9cf0b38
Gabriel Lantz <gabriel@Gabriel-Lantzs-iMac-9.local>
2016-02-13 00:16:25 +0100

    New stuff

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser());
 var path = require('path');
 var compression = require('compression');
 var swig = require('swig');
