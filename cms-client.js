21a8e596dd98173b2340c2988e1a6e4506f46ba8
Gabriel Lantz <lantzgabriel@gmail.com>
2016-04-16 14:47:33 +0200

    New stuff

--- cms-client.js
+++ cms-client.js
@@ -1,14 +1,14 @@
 if(process.env.NODE_ENV) {
   var cms = require('./bower_components/sdk/index.js');
   var client = new cms.Client({
-    bucket: process.env.STORE_ID,
+    bucket: process.env.BUCKET_ID,
     accessToken: process.env.ACCESS_TOKEN
   });
 } else {
   var cms = require('../sdk/index.js');
   var client = new cms.Client({
     host: 'localhost',
-    bucket: process.env.STORE_ID,
+    bucket: process.env.BUCKET_ID,
     accessToken: process.env.ACCESS_TOKEN,
     port: '8080',
     secure: false
